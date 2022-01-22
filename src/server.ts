23f278c7d79799f6278116b743b06e0a86ec47cb
Amit vaknin <boinxi@gmail.com>
2022-01-22 14:52:07 +0200

    made it work

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,11 @@ import { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Application works, amit is your king');
+  res.send('<h1>👑👑👑👑👑👑👑👑👑👑👑👑👑👑👑👑👑</h1>' +
+      '<h1>👑👑Application works, amit is your king!!!!!!!👑👑</h1> ' +
+      '<h1>👑👑👑👑👑👑👑👑👑👑👑👑👑👑👑👑👑</h1>' +
+      '<h2>Server is live!!</h2>' +
+      '<iframe src="https://giphy.com/embed/SggILpMXO7Xt6" width="480" height="262" allowFullScreen/>');
 });
 
 app.listen(process.env.PORT || 5000, () => {
