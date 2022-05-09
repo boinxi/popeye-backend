42c509b8583a35f89ed8310787c95df04b06c08e
Amit vaknin <boinxi@gmail.com>
2022-05-09 17:41:45 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,21 @@ const LoginRouter = require('./routes/login_route');
 
 const app = express();
 app.use(express.json())
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
 app.use('/signup', SignupRouter);
 app.use('/login', LoginRouter);
 
